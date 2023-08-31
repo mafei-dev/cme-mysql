ab48fa6d14b8e2e1b5c3b06ba9b81bfc09393afc
mafei <mafei.dev@gmail.com>
2023-08-31 18:06:14 +0530

    init

---
+++ src/main/java/com/zoftstack/cmemysql/CmeMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.zoftstack.cmemysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmeMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmeMysqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zoftstack/cmemysql/CmeMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoftstack.cmemysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmeMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
